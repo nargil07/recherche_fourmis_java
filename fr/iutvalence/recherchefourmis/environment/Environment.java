7e0779ca68ae77f381ed17ed44d9db9628a8979c
nargil07 <nargilcorp@gmail.com>
2016-05-13 09:05:23 +0200

    Ajout des classe de l'environment

---
+++ fr/iutvalence/recherchefourmis/environment/Environment.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.iutvalence.recherchefourmis.environment;
+
+/**
+ *
+ * @author antony
+ */
+public class Environment {
+    
+}

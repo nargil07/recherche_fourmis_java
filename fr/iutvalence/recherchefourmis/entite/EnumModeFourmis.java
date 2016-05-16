f66d68fae3fae47e754a582f36d17a19634d19b0
nargil07 <nargilcorp@gmail.com>
2016-05-16 22:53:06 +0200

    Modification de l'enum qui contient les pourcentage d'importance des l'heuristique ou des pheromones

--- fr/iutvalence/recherchefourmis/entite/EnumModeFourmis.java
+++ fr/iutvalence/recherchefourmis/entite/EnumModeFourmis.java
@@ -10,12 +10,41 @@ package fr.iutvalence.recherchefourmis.entite;
  * @author antony
  */
 public enum EnumModeFourmis {
+    
+    
     /**
      * 
      */
-    EXPLORATRICE,
+    EXPLORATRICE(1, 5),
     /**
      * 
      */
-    SUIVEUSE;
+    SUIVEUSE(5, 2);
+
+    private int pourcentagePheromones;
+    private int pourcentageMetrique;
+
+    private EnumModeFourmis(int pourcentagePheromones, int pourcentageMetrique) {
+        this.pourcentagePheromones = pourcentagePheromones;
+        this.pourcentageMetrique = pourcentageMetrique;
+    }
+
+    public int getPourcentagePheromones() {
+        return pourcentagePheromones;
+    }
+
+    public void setPourcentagePheromones(int pourcentagePheromones) {
+        this.pourcentagePheromones = pourcentagePheromones;
+    }
+
+    public int getPourcentageMetrique() {
+        return pourcentageMetrique;
+    }
+
+    public void setPourcentageMetrique(int pourcentageMetrique) {
+        this.pourcentageMetrique = pourcentageMetrique;
+    }
+    
+    
+    
 }
